dbd0a505f054b4ec4842995981905c063ad45e39
Camille Reynders <camille.reynders@gmail.com>
2015-09-29 13:21:52 +0200

    Finalize v1

--- accessor-object.js
+++ accessor-object.js
@@ -2,8 +2,12 @@
 
 var _ = require( 'lodash' );
 
-module.exports.create = function mixinAccessors( recipient,
-                                          accessors ){
+function createOrMixin( recipient,
+                        accessors ){
+    if( arguments.length === 1 ){
+        accessors = recipient;
+        recipient = {};
+    }
     if( !_.isObject( recipient ) || _.isArray( recipient ) ){
         throw new Error( 'Accessor Object: "recipient" must be of type "Object"' );
     }
@@ -13,14 +17,29 @@ module.exports.create = function mixinAccessors( recipient,
     _.each( accessors, function( mixed,
                                  name ){
         var processor;
-        if( _.isArray( mixed ) ){
+        if( _.isArray( mixed ) ){ // leaf with processor and initial value
             processor = mixed[ 0 ];
-            recipient[ '_' + name ] = mixed[ 1 ]; // init value
-        } else {
+            recipient[ '_' + name ] = mixed[ 1 ];
+        } else if( _.isFunction( mixed ) ){ // leaf with processor
             processor = mixed;
+        } else if( _.isObject( mixed ) ){// composite
+            recipient[ '_' + name ] = createOrMixin( mixed );
+            processor = function( value ){
+                if( !_.isObject( value ) || _.isArray( value ) ){
+                    throw new Error( 'Accessor Object: nested objects require parameters of type "Object"' );
+                }
+                _.each( value, function( mixed,
+                                         name ){
+                    this[ name ]( mixed );
+                }, recipient[ '_' + name ] );
+                return recipient[ '_' + name ];
+            }
+        } else { // leaf with initial value, falls through: boolean, string, number
+            recipient[ '_' + name ] = mixed;
+            processor = 'value';
         }
         recipient[ name ] = function accessor( value ){
-            if( arguments.length === 1 ){ // setter
+            if( arguments.length ){ // setter
                 this[ '_' + name ] = (!processor || processor === 'value')
                     ? value
                     : processor.call( this, value );
@@ -30,4 +49,6 @@ module.exports.create = function mixinAccessors( recipient,
         };
     } );
     return recipient;
-};
+}
+
+module.exports = createOrMixin; 
--- tests/accessor-object.spec.js
+++ tests/accessor-object.spec.js
@@ -8,21 +8,21 @@ var subject = require( '../accessor-object' );
 
 describe( 'AccessorObject', function(){
     it( 'should be a function', function(){
-        expect( subject.create ).to.be.a.function();
+        expect( subject ).to.be.a.function();
     } );
-    it( 'should throw when `recipient` is not an object', function(){
+    it( 'should throw when `recipient` is provided, but not an object', function(){
         expect( function(){
-            subject.create( [] );
+            subject( [], {} );
         } ).to.throw( /object/i );
     } );
     it( 'should throw when `accessors` is not an object', function(){
         expect( function(){
-            subject.create( {}, [] );
+            subject( {}, [] );
         } ).to.throw( /object/i );
     } );
-    describe( 'with vanilla objects', function(){
-        it( 'should add accessors to the recipient', function(){
-            var actual = subject.create( {}, {
+    describe( 'without recipient', function(){
+        it( 'should create an object with accessors', function(){
+            var actual = subject( {
                 foo: 'value',
                 bar: 'value'
             } );
@@ -31,14 +31,14 @@ describe( 'AccessorObject', function(){
         } );
         it( 'should allow setting and retrieving a value', function(){
             var value = {};
-            var actual = subject.create( {}, {
+            var actual = subject( {
                 foo: 'value'
             } );
             actual.foo( value );
             expect( actual.foo() ).to.equal( value );
         } );
         it( 'should allow setting a boolean value', function(){
-            var actual = subject.create( {}, {
+            var actual = subject( {
                 foo: 'value'
             } );
             actual.foo( false );
@@ -46,7 +46,7 @@ describe( 'AccessorObject', function(){
         } );
         it( 'should allow processing a value, before storage', function(){
             var value = {};
-            var actual = subject.create( {}, {
+            var actual = subject( {
                 foo: function( value ){
                     value.isProcessed = true;
                     return value;
@@ -55,10 +55,56 @@ describe( 'AccessorObject', function(){
             actual.foo( value );
             expect( value.isProcessed ).to.be.true();
         } );
+        it( 'should allow setting an initialization value', function(){
+            var value = 'initialized';
+            var actual = subject( {
+                foo: value
+            } );
+            expect( actual.foo() ).to.equal( value );
+        } );
+        it( 'should allow setting a processor _and_ an initialization value', function(){
+            var value = 'initialized';
+            var actual = subject( {
+                foo: [ 'value', value ]
+            } );
+            expect( actual.foo() ).to.equal( value );
+        } );
+        it( 'should allow setting a `null` value', function(){
+            var actual = subject( {
+                foo: [ 'value', 'a value' ]
+            } );
+            actual.foo( null );
+            expect( actual.foo() ).to.equal( null );
+        } );
+        it( 'should allow setting an `undefined` value', function(){
+            var actual = subject( {
+                foo: [ 'value', 'a value' ]
+            } );
+            actual.foo( undefined );
+            expect( actual.foo() ).to.equal( undefined );
+        } );
+        it( 'should return the instance when used as a setter', function(){
+            var actual = subject( {
+                foo: 'value'
+            } );
+
+            expect( actual.foo( 'whatever value' ) ).to.equal( actual );
+        } );
+    } );
+    describe( 'with vanilla objects', function(){
+        it( 'should add accessors to the recipient', function(){
+            var actual = {};
+            subject( actual, {
+                foo: 'value',
+                bar: 'value'
+            } );
+            expect( actual.foo ).to.be.a.function();
+            expect( actual.bar ).to.be.a.function();
+        } );
         it( 'should execute processors in the `recipient` context', function(){
             var recipient = {};
             var actual;
-            subject.create( recipient, {
+            subject( recipient, {
                 foo: function( value ){
                     actual = this;
                     return value;
@@ -66,34 +112,6 @@ describe( 'AccessorObject', function(){
             } );
             recipient.foo( 'a value' );
             expect( actual ).to.equal( recipient );
-        } );
-        it( 'should allow setting an initialization value', function(){
-            var value = 'initialized';
-            var actual = subject.create( {}, {
-                foo: [ 'value', value ]
-            } );
-            expect( actual.foo() ).to.equal( value );
-        } );
-        it( 'should allow setting a `null` value', function(){
-            var actual = subject.create( {}, {
-                foo: [ 'value', 'a value' ]
-            } );
-            actual.foo( null );
-            expect( actual.foo() ).to.equal( null );
-        } );
-        it( 'should allow setting an `undefined` value', function(){
-            var actual = subject.create( {}, {
-                foo: [ 'value', 'a value' ]
-            } );
-            actual.foo( undefined );
-            expect( actual.foo() ).to.equal( undefined );
-        } );
-        it( 'should return the instance when used as a setter', function(){
-            var actual = subject.create( {}, {
-                foo: 'value'
-            } );
-
-            expect( actual.foo( 'whatever value' ) ).to.equal( actual );
         } );
     } );
     describe( 'with prototypes', function(){
@@ -103,7 +121,7 @@ describe( 'AccessorObject', function(){
             };
         } );
         it( 'should add accessors to the prototype', function(){
-            subject.create( Class.prototype, {
+            subject( Class.prototype, {
                 foo: 'value',
                 bar: 'value'
             } );
@@ -112,7 +130,7 @@ describe( 'AccessorObject', function(){
             expect( actual.bar ).to.be.a.function();
         } );
         it( 'should create instance properties', function(){
-            subject.create( Class.prototype, {
+            subject( Class.prototype, {
                 foo: 'value'
             } );
             var actual1 = new Class();
@@ -123,7 +141,7 @@ describe( 'AccessorObject', function(){
         } );
         it( 'should execute processors in the instance context', function(){
             var actual;
-            subject.create( Class.prototype, {
+            subject( Class.prototype, {
                 foo: function( value ){
                     actual = this;
                     return value;
@@ -134,14 +152,38 @@ describe( 'AccessorObject', function(){
             expect( actual ).to.equal( instance );
         } );
     } );
-    describe.skip( 'nesting', function(){
-        it( '', function(){
-            var actual = subject.create( {}, {
-                'foo.bar': [
-                    'value', 'a value'
-                ]
+    describe( 'nesting', function(){
+        it( 'should set up nested accessor objects', function(){
+            var actual = subject( {
+                foo: {
+                    bar: {
+                        qux: [ 'value', 'qux' ]
+                    },
+                    baz: [ 'value', 'baz' ]
+                }
             } );
-            expect( actual.foo().bar() ).to.equal( 'a value' );
+            expect( actual.foo().bar().qux() ).to.equal( 'qux' );
+            expect( actual.foo().baz() ).to.equal( 'baz' );
+        } );
+        it( 'should allow setting values through an object middle of the path', function(){
+            var actual = subject( {
+                foo: {
+                    bar: {
+                        qux: [ 'value', 'qux' ],
+                        whatever: [ 'value', 'whatever' ]
+                    },
+                    baz: [ 'value', 'baz' ]
+                }
+            } );
+            actual.foo( {
+                bar: {
+                    qux: 'changed qux'
+                },
+                baz: 'changed baz'
+            } );
+            expect( actual.foo().bar().qux() ).to.equal( 'changed qux' );
+            expect( actual.foo().bar().whatever() ).to.equal( 'whatever' );
+            expect( actual.foo().baz() ).to.equal( 'changed baz' );
         } );
     } );
 } );
